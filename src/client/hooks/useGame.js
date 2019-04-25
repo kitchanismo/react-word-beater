9c4cb4c86aab192b4abed254092a8edb0f82a8e1
kitchanismo <kitchanismo@yahoo.com>
2019-04-25 02:01:59 -0700

    old implementing points

--- src/client/hooks/useGame.js
+++ src/client/hooks/useGame.js
@@ -1,54 +1,34 @@
 import { useState, useEffect } from 'react'
 
-export const useGame = ({ isMatched, length }) => {
+export const useGame = ({ isMatched }) => {
   const DEFAULT_TIMER = 20
   const [score, setScore] = useState(0)
   const [level, setLevel] = useState(1)
-  const [points, setPoints] = useState(0)
+  const [points, setPoints] = useState(10)
   const [timerBase, setTimerBase] = useState(DEFAULT_TIMER)
 
-  const handleLevelUp = (tb, lvl) => {
+  const handleLevelUp = (tb, lvl, pts = 10) => {
     setTimerBase(tb)
     setLevel(lvl)
-  }
-
-  // points base on word length
-  const generatePoints = length => {
-    if (length >= 12) {
-      return 25
-    } else if (length >= 9) {
-      return 20
-    } else if (length >= 6) {
-      return 15
-    } else if (length <= 5) {
-      return 10
-    }
+    setPoints(pts)
   }
 
   useEffect(() => {
     if (isMatched) {
       if (score >= 200) {
-        handleLevelUp(3, 6) // lvl 6: 3s
+        handleLevelUp(3, 6, 25) // lvl 6: 3s
       } else if (score >= 125) {
-        handleLevelUp(6, 5) // lvl 5: 6s
+        handleLevelUp(6, 5, 20) // lvl 5: 6s
       } else if (score >= 100) {
-        handleLevelUp(9, 4) // lvl 4: 9s
+        handleLevelUp(9, 4, 18) // lvl 4: 9s
       } else if (score >= 75) {
-        handleLevelUp(12, 3) // lvl 3: 12s
+        handleLevelUp(12, 3, 15) // lvl 3: 12s
       } else if (score >= 50) {
-        handleLevelUp(15, 2) // lvl 2: 15s
+        handleLevelUp(15, 2, 13) // lvl 2: 15s
       }
 
-      if (length >= 12) {
-        setPoints(25)
-      } else if (length >= 6) {
-        setPoints(20)
-      } else if (length >= 4) {
-        setPoints(15)
-      } else setPoints(10)
+      setScore(score + points)
     }
-
-    setScore(score + points)
 
     console.log(points)
   }, [isMatched])
