b7f2f5f1a520adcccf52248c8877189083e55d0c
kitchanismo <kitchanismo@yahoo.com>
2019-04-26 06:07:56 -0700

    dist deploy as well

---
+++ dist/precache-manifest.e7535f76092dde4bd91f70b19afc226b.js
@@ -0,0 +1,33 @@
+self.__precacheManifest = [
+  {
+    "revision": "62ecd88c71a0efb220469c290d194726",
+    "url": "/static\\favicon.png"
+  },
+  {
+    "url": "/media/happy.e0e231ef16c54cd8ce3a601393f8ad4d.mp3"
+  },
+  {
+    "url": "/media/gameover.4a5a2fae6ac11096c345a4d88a666ac5.mp3"
+  },
+  {
+    "url": "/media/correct.553b258171193dba2cbd915043f92fe2.mp3"
+  },
+  {
+    "url": "/js/main.a1f5e1efe95c93c2f76c.js"
+  },
+  {
+    "url": "/js/1.e470bb07e9c46f451521.js"
+  },
+  {
+    "revision": "b4c3714d9e1f93cd1e74e4465b35b002",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e470bb07e9c46f451521",
+    "url": "/css/vendor.016be89688236ecd24cb_1.css"
+  },
+  {
+    "revision": "a1f5e1efe95c93c2f76c",
+    "url": "/css/main.css"
+  }
+];
